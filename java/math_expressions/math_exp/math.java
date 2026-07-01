b6100dfe441f05105d6403103bf043d92b99a46b
Adalberto Machin <70716647+Adalberto-Machin@users.noreply.github.com>
2026-07-01 08:38:52 -0500

    finished first week of java

---
+++ java/math_expressions/math_exp/math.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of math here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class math {
+int g (int a) {
+  if (a < 9) {
+    return 9;
+  }
+  
+  if (a < 7) {
+    return 7;
+  }
+  
+  if (a < 4) {
+    return 4;
+  }
+  
+  return 0;
+}
+}
