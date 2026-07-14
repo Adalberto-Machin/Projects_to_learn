969b14cae13db32b473c04d685ce656b599072f7
Adalberto Machin <70716647+Adalberto-Machin@users.noreply.github.com>
2026-07-14 08:08:16 -0500

    almost done with part 3

--- java/StringsThirdAssignments/StringsThirdAssignments/Part1.java
+++ java/StringsThirdAssignments/StringsThirdAssignments/Part1.java
@@ -57,7 +57,69 @@ public class Part1 {
     }
 
 
-        public cgRatio
+        public double cgRatio (String dna){
+            // this method returns ratio of C’s and G’s in dna as a fraction of the entire strand of DNA
+            // make sure you convert the count of Cs and Gs into float to make sure you get a float result in the division
+            int c_index = 0;
+            int g_index = 0;
+            int found_index = 0; // temporary index holder used to see if an index is found in the data
+            dna = dna.toLowerCase();
+            int c_count = 0;
+            int g_count = 0;
+            
+            System.out.println("DNA strand to analyze is  " + dna);
+            // find the number of times C occurs
+            while (found_index != -1){
+                found_index = dna.indexOf("c", c_index);
+                c_index = found_index + 1;
+                if (found_index != -1){
+                    c_count = c_count + 1;
+                }
+                
+                System.out.println("C count is  " + c_count);
+            }
+            
+            // reset found index back to 0 to find G
+            found_index = 0; 
+            
+            // find the number of times G occurs
+            while (found_index != -1){
+                found_index = dna.indexOf("g", g_index);
+                g_index = found_index + 1;
+                if (found_index != -1){
+                    g_count = g_count + 1;
+                }
+                
+                System.out.println("G count is  " + g_count);
+            }
+            
+            double cgratio_result = ((double) (c_count + g_count)) / dna.length();
+            
+            return cgratio_result;
+        }
+        
+        public int countCTG(String dna){
+            // returns the number of times the codon CTG appears in DNA
+            int ctg_index = 0;
+            int found_index = 0; // temporary index holder used to see if an index is found in the data
+            dna = dna.toLowerCase();
+            int ctg_count = 0;
+            
+            System.out.println("DNA strand to analyze is  " + dna);
+            // find the number of times C occurs
+            while (found_index != -1){
+                found_index = dna.indexOf("ctg", ctg_index);
+                ctg_index = found_index + 1;
+                if (found_index != -1){
+                    ctg_count = ctg_count + 1;
+                }
+                
+                //System.out.println("CTG count is  " + ctg_count);
+            }
+            
+            return ctg_count;
+        }
+    
         public StorageResource getAllGenes (String dna){
         // this method will find all of the genes in the DNA strand
         StorageResource geneList = new StorageResource();
@@ -79,7 +141,33 @@ public class Part1 {
         return geneList;
     }
     
-    
+    public void processGenes (StorageResource sr){
+        int count_9 = 0;
+        int count_cg_ratio = 0;
+        int current_length = 0;
+        int max_length = 0;
+        double cg_ratio_result = 0;
+        for (String dna: sr.data()){
+            if (dna.length() > 9){
+                count_9 = count_9 + 1;
+                System.out.println("String with count greater than 9 found, which is " + dna);
+            }
+            // get the C-G ratio 
+            cg_ratio_result = cgRatio(dna);
+            if (cg_ratio_result > 0.35){
+                System.out.println("String with c-g ratio greater than 0.35 found, which is " + dna);
+                count_cg_ratio = count_cg_ratio + 1;
+            }
+            //track the length of the longest gene in sr
+            current_length = dna.length();
+            if (current_length > max_length){
+                max_length = current_length;            
+            }
+        }
+        System.out.println("number of Strings in sr that are longer than 9 characters is " + count_9);
+        System.out.println("number of Strings in sr that have c-g ratio higher than 0.35 " + count_cg_ratio);
+        System.out.println("Longest gene is " + max_length);
+    }
     
         public void testgetAllGenes(){
         List<String> all_cases = new ArrayList<>();
@@ -108,6 +196,26 @@ public class Part1 {
         }
         System.out.println("Complete ");
 
+    }
+    
+    public void test_cgRatio(){
+        String test_dna = "ATGCCATAG";
+        
+        System.out.println("Testing cgRatio");
+        double ratio = cgRatio(test_dna);
+        System.out.println("G and C ratio in this DNA is  " + ratio);
+        
+        System.out.println("Testing countCTG");
+        int ctg_count = countCTG(test_dna);
+        System.out.println("GTC count is  " + ctg_count);
+        
+        test_dna = "ATGCCATAGCTGCTGwsijurghbsiugbCTG";
+        ctg_count = countCTG(test_dna);
+        System.out.println("CTG count is  " + ctg_count);
+    }
+    
+    public void testProcessGenes(){
+    // this method will test the process genes method
     }
 }
 
